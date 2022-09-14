266b3afd61881685c86e8c603003cd766c0ec422
saschmallow <73910966+saschmallow@users.noreply.github.com>
2022-09-14 18:15:17 +0000

    Update traffic_light_car.ino

--- traffic_light_car.ino
+++ traffic_light_car.ino
@@ -1,6 +1,7 @@
-include <Time.h>
+#include <Time.h>
 
 time_t deadline;
+boolean is_light_on;
 
 void setup() {
   // put your setup code here, to run once:
@@ -8,13 +9,31 @@ void setup() {
   pinMode(11,OUTPUT);
   pinMode(10,OUTPUT);
   pinMode(2,INPUT);
-  deadline = millis()+10000;
-  digitalWrite(12,HIGH);
+  deadline = millis()+500;
+  digitalWrite(11,LOW);
+  is_light_on = false;
+  Serial.begin(9600);
 }
  
 void loop(){
-  if (button_pressed()) {
-    if (deadline-millis()>5000) {
+  if (millis() > deadline) {
+    if (is_light_on) {
+      Serial.println("TO OFF");
+      digitalWrite(11,LOW);
+      deadline = millis()+500;
+      is_light_on = false;
+    } else {
+      Serial.println("TO ON");
+      digitalWrite(11,HIGH);
+      deadline = millis()+500;
+      is_light_on = true;
+    }
+  }
+}
+
+void old_loop(){
+    if (button_pressed()) {                // when the button is pressed, check the time that's left. If it's more than 5s, set the deadline to 5s.   
+    if (deadline-millis()>5000) {        // otherwise the deadline remains unchanged
       deadline = millis()+5000;
     }
   }
